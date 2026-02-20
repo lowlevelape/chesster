e591f0551a94467772d62e04f71e4125be7ec278
kneelptr <nilgabriel365@gmail.com>
2026-02-20 23:00:04 +0800

    feat: add initial board layout.
    Added an initial FEN layout to board and also a background.

--- chessboard/components/board.tsx
+++ chessboard/components/board.tsx
@@ -1,11 +1,21 @@
 "use client";
+
 import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import { useState } from "react";
 
 export default function Board() {
+  const [game, setGame] = useState(new Chess());
+  const chessboardOptions = {
+    id: "basicBoard",
+    position: game.fen(),
+  };
   return (
-    <section className="pt-5">
-      <div className="w-200 m-auto">
-        <Chessboard />
+    <section className="relative transition-all duration-500">
+      <div
+        className={`rounded-lg overflow-hidden shadow-[0_20px_50px_rgba(0,0,0,0.5)] border border-slate-700/50 w-200`}
+      >
+        <Chessboard options={chessboardOptions} />
       </div>
     </section>
   );
